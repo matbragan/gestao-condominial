8a05a34f8aef0ee3e74292388e4fe5137b2b1ecc
MatOliveir <matheus.bs.oliveira@gmail.com>
2023-11-21 22:43:06 -0300

    fixing load tables in bigquery

--- utils/bigquery.py
+++ utils/bigquery.py
@@ -1,11 +1,15 @@
 import os 
+import logging
 from google.cloud import bigquery
 
 from utils.google_storage import list_files
 from utils import GOOGLE_CREDENTIALS, PROJECT_ID, BUCKET_NAME
+from etl import TABLES
+
+log_format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=log_format, level=logging.INFO)
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GOOGLE_CREDENTIALS
-
 
 def load_tables(
         layer: str,
@@ -22,27 +26,24 @@ def load_tables(
     )
 
     bucket_files = list_files(bucket_name)
-    
-    layer_files = []
 
     for file in bucket_files:
         split_file = file.split('/')
-        if split_file[0] == layer and split_file[2].endswith('.csv'):
-            layer_files.append(file)
+        dataset = split_file[0]
+        table = split_file[-1].replace('.csv', '')
         
-    for file in layer_files:
-        uri = f'gs://{bucket_name}/{file}'
+        if dataset == layer and table in TABLES:
+            uri = f'gs://{bucket_name}/{file}'
+            table_id = f'{project_id}.{dataset}.{table}'
+            
+            try:
+                load_job = client.load_table_from_uri(
+                    uri, table_id, job_config=job_config
+                )
 
-        split_file = file.split('/')
-        schema = split_file[1]
-        table = split_file[2].replace('.csv', '')
-        table_id = f'{project_id}.{schema}.{table}'
+                load_job.result()
 
-        load_job = client.load_table_from_uri(
-            uri, table_id, job_config=job_config
-        )
-
-        load_job.result()
-
-        destination_table = client.get_table(table_id)
-        print(f'Tabela {schema}.{table} carregada para BigQuery, com {destination_table.num_rows} linhas.')
+                destination_table = client.get_table(table_id)
+                logging.info(f'{dataset}.{table} Load to BigQuery, with {destination_table.num_rows} rows.')
+            except Exception as e:
+                logging.warning(f'{dataset}.{table} Exception to Load to BigQuery: {e}')
